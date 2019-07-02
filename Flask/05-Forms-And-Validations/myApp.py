bd028159690a0dcd4a3defbad77258524ca8dc62
Asapanna Rakesh <45640029+rakesh4real@users.noreply.github.com>
2019-07-02 19:14:10 +0530

    Secret key and validation added

--- Flask/05-Forms-And-Validations/myApp.py
+++ Flask/05-Forms-And-Validations/myApp.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+# Used to generate secured cookies to avoid vulnerabilities
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
