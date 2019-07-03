ef56b47bfc03d0286c4238489c469c6083b49add
Asapanna Rakesh <45640029+rakesh4real@users.noreply.github.com>
2019-07-03 11:21:11 +0530

    Create __init__.py

---
+++ Flask/07-Conversion-To-Package/myapp/__init__.py
@@ -0,0 +1,11 @@
+# Only include app, import necessary packages for creation of app and import routes 
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+from flaskblog import routes
